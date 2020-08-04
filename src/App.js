3edd9c0dde0c1d7bfbc4499a95f7aba1e4fdc251
Ruben Mateus <ruben.mateus3@gmail.com>
2020-08-05 00:00:24 +0100

    add first iteration of grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { css } from 'emotion'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Tic Tac Toe</h1>
       </header>
+      <Game />
     </div>
-  );
+  )
+}
+
+function generateGrid(rows, columns, mapper) {
+  return Array(rows).fill().map(() => Array(columns).fill().map(mapper));
+}
+
+const grid = generateGrid(3,3,() => null);
+
+function Game() {
+  return <div><Grid grid={grid}/></div>
+}
+
+function Grid({grid}){
+  return (
+    <div className={css`display: inline-block`}>
+      <div className={css`
+        background-color: #444;
+        display:grid;
+        grid-template-rows: repeat(${grid.length}, 1fr);
+        grid-template-columns: repeat(${grid[0].length}, 1fr);
+        grid-gap: 2px;
+        `}
+      >
+        {grid.map((row, rowIndex) => row.map(
+          (column, colIndex) =>
+            <Cell key={`${colIndex}-${rowIndex}`} value={column} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function Cell({value}){
+  return <div className={css`
+    background-color: #fff;
+    width: 100px;
+    height: 100px;
+    `
+  }>
+
+  </div>
 }
 
 export default App;
