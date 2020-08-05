864ce314f68a9af23b5cfd01ef163e685d5b6d65
Ruben Mateus <ruben.mateus3@gmail.com>
2020-08-05 01:59:02 +0100

    add core logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { css } from 'emotion'
 
 const App = () => {
@@ -12,17 +12,131 @@ const App = () => {
   )
 }
 
+const clone = x => JSON.parse(JSON.stringify(x));
+
 function generateGrid(rows, columns, mapper) {
   return Array(rows).fill().map(() => Array(columns).fill().map(mapper));
 }
 
-const grid = generateGrid(3,3,() => null);
+const grid = () => generateGrid(3,3,() => null);
 
-function Game() {
-  return <div><Grid grid={grid}/></div>
+function checkTree(a,b,c) {
+  if(!a || !b || !c) {
+    return false;
+  }
+
+  return a === b && b === c;
 }
 
-function Grid({grid}){
+function checkForWin(flatGrid) {
+  const [nw, n, ne, w, c, e, sw, s, se] = flatGrid;
+
+  return (
+    checkTree(nw, n, ne) ||
+    checkTree(w, c, e) ||
+    checkTree(sw, s, se) ||
+    checkTree(nw, w, sw) ||
+    checkTree(n, c, s) ||
+    checkTree(ne, e, se) ||
+    checkTree(nw, c, se) ||
+    checkTree(ne, c, sw)
+  );
+}
+
+function checkForDraw(flatGrid) {
+  return !checkForWin(flatGrid) && flatGrid.filter(Boolean).length === flatGrid.length;
+}
+
+const NEXT_TURN = {
+  O : 'X',
+  X : 'O'
+}
+
+const getInitialState = () => ({
+  grid: grid(),
+  turn: 'X',
+  status: 'inProgress'
+});
+
+const reducer = (state, action) => {
+  if(state.status === 'success' && action.type !== 'RESET') {
+    return state;
+  }
+
+  switch(action.type){
+    case 'RESET':
+      return getInitialState();
+    case 'CLICK': {
+      const { x, y } = action.payload;
+      const { grid, turn } = state;
+
+      if(grid[y][x]){
+        return state;
+      }
+
+      console.log(state);
+
+      const nextState = clone(state);
+
+      nextState.grid[y][x] = turn;
+
+      const flatGrid = nextState.grid.flat();
+
+      if(checkForWin(flatGrid)) {
+        nextState.status = 'success';
+        return nextState;
+      }
+      if(checkForDraw(flatGrid)){
+        return getInitialState();
+      }
+
+      nextState.turn = NEXT_TURN[turn];
+
+      return nextState;
+    }
+
+    default:
+      return state;
+  }
+}
+
+function Game() {
+  const [state, dispatch] = useReducer(reducer, getInitialState())
+  const { grid, status, turn } = state;
+
+  const handleClick = (x, y) => {
+    dispatch({ type: 'CLICK', payload: { x, y }});
+  };
+
+  const reset = () => {
+    dispatch({ type: 'RESET'});
+  }
+
+  return (
+    <div className={css`
+      display: inline-block;
+
+    `}>
+      <div className={css`
+        display: flex;
+        justify-content: space-between;
+      `}>
+        <div>
+          Next Turn : {turn}
+        </div>
+        <div>
+          { status === 'success' ? `${turn} won!` : null}
+        </div>
+        <button onClick={reset} type="button">
+          Reset
+        </button>
+      </div>
+      <Grid grid={grid} onClick={handleClick}/>
+    </div>
+  )
+}
+
+function Grid({grid, onClick}){
   return (
     <div className={css`display: inline-block`}>
       <div className={css`
@@ -35,21 +149,24 @@ function Grid({grid}){
       >
         {grid.map((row, rowIndex) => row.map(
           (column, colIndex) =>
-            <Cell key={`${colIndex}-${rowIndex}`} value={column} />
+            <Cell key={`${colIndex}-${rowIndex}`} value={column} onClick={() => onClick(colIndex, rowIndex)}/>
         ))}
       </div>
     </div>
   )
 }
 
-function Cell({value}){
+function Cell({value, onClick}){
   return <div className={css`
     background-color: #fff;
     width: 100px;
     height: 100px;
-    `
-  }>
-
+    `}
+  >
+    <button type="button" onClick={onClick} className={css`
+      width: 100%;
+      height: 100%;
+    `}>{value}</button>
   </div>
 }
 
