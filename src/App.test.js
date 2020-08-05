7d4629298bf3288ba06ddc96946c3d764072f3e4
Ruben Mateus <ruben.mateus3@gmail.com>
2020-08-06 00:08:42 +0100

    add prettier configs, tests and deps

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  test('renders correctly', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('renders title of the game', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Tic Tac Toe/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
