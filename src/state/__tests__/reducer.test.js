b62638d0a95f4e6d9f355aa5ed379f313ea45bb5
Ruben Mateus <ruben.mateus3@gmail.com>
2020-08-31 01:42:55 +0100

    simple tests added

--- src/state/__tests__/reducer.test.js
+++ src/state/__tests__/reducer.test.js
@@ -1,12 +1,34 @@
 import { reducer } from '../reducer';
+import { generateGrid } from '../../utils';
 
 describe('reducer', () => {
-  test('Should state when status is success and type is not RESET', () => {
+  test('Should return state when status is success and type is not RESET', () => {
     const state = { status: 'success' };
     const action = { type: 'CLICK' };
     const result = reducer(state, action);
 
     expect(result).not.toBeNull();
-    expect(state).toStrictEqual({ status: 'success' });
+    expect(result).toStrictEqual({ status: 'success' });
+  });
+
+  test('Should return state when type does not exist', () => {
+    const state = { status: 'success' };
+    const action = { type: 'TEST' };
+    const result = reducer(state, action);
+
+    expect(result).not.toBeNull();
+    expect(result).toStrictEqual({ status: 'success' });
+  });
+
+  test('Should return initialState when type is RESET', () => {
+    const action = { type: 'RESET' };
+    const result = reducer({}, action);
+
+    expect(result).not.toBeNull();
+    expect(result).toStrictEqual({
+      grid: generateGrid(3, 3, () => null),
+      turn: 'X',
+      status: 'inProgress',
+    });
   });
 });
