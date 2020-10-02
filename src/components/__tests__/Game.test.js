72ed7649d63ba3d127615a9908dc09a18fbfb91c
Ruben Mateus <ruben.mateus3@gmail.com>
2020-10-02 01:37:35 +0100

    update deps and add more tests

--- src/components/__tests__/Game.test.js
+++ src/components/__tests__/Game.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Game from '../Game';
 
-describe('<Grid />', () => {
+describe('<Game />', () => {
   test('renders correctly', () => {
     const { asFragment } = render(<Game />);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('call reset on button Click', () => {
+    render(<Game />);
+
+    fireEvent.click(screen.getByRole('button', { name: 'Reset' }));
+
+    expect(screen.getByTestId('status').textContent).toBe('');
+  });
+
+  test('call onClick on grid', () => {
+    render(<Game />);
+
+    fireEvent.click(screen.getByTestId('grid'));
+
+    expect(screen.getByText(/next turn : x/i).textContent).toBeDefined();
   });
 });
